133898f36dc1cc02c25eb61afc5888f7be6b8a7e
Ashish Balu <140700105+Ashishbalu@users.noreply.github.com>
2026-01-06 16:49:21 +0530

    added one more

--- Pattern/p1.java
+++ Pattern/p1.java
@@ -59,8 +59,6 @@ public class p1 {
 
     public void printP7(int n) {
         for (int i = 0; i < n; i++) {
-
-
             // for space
             for (int j = 0; j < n - i - 1; j++) {
                 System.out.print(" ");
@@ -76,6 +74,7 @@ public class p1 {
             System.out.println();
         }
     }
+    
     public void printP8(int n) {
         for (int i = 0; i < n; i++) {
             // for space
@@ -93,12 +92,26 @@ public class p1 {
             System.out.println();
         }
     }
+    
+    //For the printP9 pattern merge the P7&P8 call them together
+    
+    void printP10(int n){
+        for (int i = 1; i < (2*n-1) ; i++) {
+            int stars = i;
+            if (i>n){
+                stars = 2*n-i;
+            }
+            for (int j = 1; j < stars ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
         p1 obj = new p1();
-        obj.printP8(5);
-//        System.out.println();
-//        obj.printP7(6);
+        obj.printP10(5);
+//        obj.printP8(5);
 
     }
 }
