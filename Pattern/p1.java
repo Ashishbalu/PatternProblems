a559d8746b9a8d02d5869500c00db23519d4304b
Ashish Balu <140700105+Ashishbalu@users.noreply.github.com>
2026-01-06 15:47:56 +0530

    updted with new pattern problem

--- Pattern/p1.java
+++ Pattern/p1.java
@@ -57,11 +57,31 @@ public class p1 {
 
     }
 
+    public void printP7(int n) {
+        for (int i = 0; i < n; i++) {
+
+
+            // for space
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            //for star
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            //for space
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         p1 obj = new p1();
-        obj.printP6(4);
+        obj.printP7(5);
         System.out.println();
-        obj.printP6(5);
+        obj.printP7(6);
 
     }
 }
